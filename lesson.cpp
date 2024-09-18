6718ebcb878a321f57f3b3a9cea5b6246848b71c
appleaww <artemmorozz2006@gmail.com>
2024-09-18 21:44:41 +0300

    Create lesson.cpp

---
+++ lesson.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+int b;
+int c;
+float S;
+float p;
+std::cout << "Введите строну a\n";
+std::cin >> a;
+std::cout << "Введите строну b\n";
+std::cin >> b;
+std::cout << "Введите строну c\n";
+std::cin >> c;
+p = 0.5*(a+b+c);
+if (a+b<=c or a+c<=b or c+b<=a)
+{
+    std::cout<<"Такого треугольника не существует\n";
+}
+else {
+S = sqrt(p*(p-a)*(p-b)*(p-c));
+std::cout<<"S = "<< S;
+}
+    return 0;
+}
