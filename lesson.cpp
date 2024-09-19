f450684bc5303a728573a9be35f0ebf2d1e829d3
appleaww <artemmorozz2006@gmail.com>
2024-09-20 00:00:42 +0300

    Entering two integers

--- lesson.cpp
+++ lesson.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
 int a;
 int b;
+int n;
+int m;
 int c;
 float S;
 float p;
@@ -13,6 +15,10 @@ std::cout << "Введите строну b\n";
 std::cin >> b;
 std::cout << "Введите строну c\n";
 std::cin >> c;
+std::cout << "Введите число m\n";
+std::cin >> m;
+std::cout << "Введите число n\n";
+std::cin >> n;
 p = 0.5*(a+b+c);
 if (a+b<=c or a+c<=b or c+b<=a)
 {
@@ -22,7 +28,7 @@ else {
 S = sqrt(p*(p-a)*(p-b)*(p-c));
 std::cout<<"S = "<< S << "\n";
 }
-if ( a= b= c )
+if ( a==b and c==b and c==a  )
 {
     std::cout<<"Треугольник равносторонний\n";
 }
